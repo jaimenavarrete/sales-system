89addd1f49e0cd183b503aba3db8d6d80fa72d30
Jaime Navarrete <jaimenava123@outlook.com>
2023-02-21 13:37:22 -0600

    Add Products controller and view. Make some design changes in layout
    Changes default names and titles

---
+++ SalesSystem/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SalesSystem.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
