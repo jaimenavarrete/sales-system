274039ebab4da383c9be191dad6f17688ed94ba1
Jaime Navarrete <jaimenava123@outlook.com>
2023-03-02 10:40:36 -0600

    Add categories visualization. Change categories table styles
    Creates categories service to communicate with database.
    Now the categories table indicate when a category does not have description.

---
+++ SalesSystem.Business/Services/CategoriesService.cs
@@ -0,0 +1,18 @@
+﻿using SalesSystem.DataAccess.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesSystem.Business.Services
+{
+    public class CategoriesService
+    {
+        private readonly SalesSystemEntities _context = new SalesSystemEntities();
+
+        public List<Categories> GetCategories()
+        {
+            var categories = _context.Categories.ToList();
+
+            return categories;
+        }
+    }
+}
--- SalesSystem.Presentation/Controllers/CategoriesController.cs
+++ SalesSystem.Presentation/Controllers/CategoriesController.cs
@@ -1,18 +1,32 @@
-﻿using SalesSystem.Presentation.Models.ViewModels.Categories;
+﻿using SalesSystem.Business.Services;
+using SalesSystem.Presentation.Models.ViewModels.Categories;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace SalesSystem.Presentation.Controllers
 {
     public class CategoriesController : Controller
     {
+        private readonly CategoriesService _categoriesService = new CategoriesService();
+
         // GET: Categories
         public ActionResult Index()
         {
-            var viewModel = new List<CategoryViewModel>()
-            {
-                new CategoryViewModel() { Id = 1, Name = "Categoría 1", Description = "Descripción 1" }
-            };
+            var categories = _categoriesService.GetCategories();
+
+            var viewModel = categories
+                .Select(category => new CategoryViewModel()
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    Description = category.Description,
+                    Created = category.Created,
+                    CreatedBy = category.CreatedBy,
+                    Modified = category.Modified,
+                    ModifiedBy = category.ModifiedBy
+                })
+                .ToList();
 
             return View(viewModel);
         }
